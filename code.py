b5b9229c5f48a4d3a8b9511960e0c6f8da0e52cb
Yasmine Hamdy <yasminehamdy@aucegypt.edu>
2022-04-11 13:17:04 +0200

    import libraries

--- code.py
+++ code.py
@@ -4,3 +4,14 @@
 # Implement the new webpage,
 # Keep the old webpage, or
 # Perhaps run the experiment longer to make their decision.
+
+# Part I - Probability
+# import libraries
+
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+%matplotlib inline
+#We are setting the seed to assure you get the same answers on quizzes as we set up
+random.seed(42)
